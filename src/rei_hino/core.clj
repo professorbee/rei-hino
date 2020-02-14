b33080ed98368b1de7ef3b5f4a011cd4acbdc72d
professorbee <bea@coyotea.xyz>
2020-02-13 19:40:23 -0500

    Add some real bad code.

--- src/rei_hino/core.clj
+++ src/rei_hino/core.clj
@@ -1,7 +1,18 @@
 (ns rei-hino.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.shell :as shell]))
+
+(defn cowprint [speak]
+  (->> (shell/sh "cowsay" speak) :out print))
+
+; This is awful practice
+(defn mjpegtime []
+  (future
+   (shell/sh "mjpg_streamer" "-i" "input_uvc.so" "-o" "output_http.so" "&")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+ "I don't do a whole lot ... yet."
+ [& args]
+ (cowprint "Jetson Time!")
+ (mjpegtime)
+ (shutdown-agents))
